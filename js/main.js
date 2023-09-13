c5e518de16fc700f312ddd4dfd81a0b3717dd330
German Novikov <difenz1k@gmail.com>
2023-09-13 19:34:04 +0300

    burger

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@ window.addEventListener('load', function() {
   });
   
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let prevScrollPos = window.pageYOffset;
-    const header = document.querySelector('.header');
+  let prevScrollPos = window.pageYOffset;
+const header = document.querySelector('.header');
 
-    window.addEventListener('scroll', () => {
-        const currentScrollPos = window.pageYOffset;
-        if (prevScrollPos > currentScrollPos) {
-            // Скроллим вверх
-            header.classList.remove('scroll-up');
-        } else {
-            // Скроллим вниз
-            header.classList.add('scroll-up');
-        }
-        prevScrollPos = currentScrollPos;
-    });
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        // Скроллим вверх
+        header.classList.remove('scroll-up');
+    } else {
+        // Скроллим вниз
+        header.classList.add('scroll-up');
+    }
+    prevScrollPos = currentScrollPos;
+});
+
+
+const burgerMenu = document.querySelector('.burger-menu');
+const headerItems = document.querySelector('.header__items ul');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    headerItems.classList.toggle('active');
 });
