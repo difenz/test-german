c60936abb080097c589627812269233fac7fd9dc
German Novikov <difenz1k@gmail.com>
2023-09-13 20:01:00 +0300

    burger

---
+++ js/loader.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    const loadingOverlay = document.querySelector('.loader');
+    loadingOverlay.style.display = 'none';
+  });
--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-window.addEventListener('load', function() {
-    const loadingOverlay = document.querySelector('.loader');
-    loadingOverlay.style.display = 'none';
-  });
-  
-
-  let prevScrollPos = window.pageYOffset;
-const header = document.querySelector('.header');
-
-window.addEventListener('scroll', () => {
-    const currentScrollPos = window.pageYOffset;
-    if (prevScrollPos > currentScrollPos) {
-        // Скроллим вверх
-        header.classList.remove('scroll-up');
-    } else {
-        // Скроллим вниз
-        header.classList.add('scroll-up');
-    }
-    prevScrollPos = currentScrollPos;
+window.addEventListener('load', function () {
+  const loadingOverlay = document.querySelector('.loader');
+  loadingOverlay.style.display = 'none';
 });
 
+let prevScrollPos = window.pageYOffset;
+const header = document.querySelector('.header');
+let isBurgerMenuOpen = false;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+  
+  if (!isBurgerMenuOpen) {
+      if (prevScrollPos > currentScrollPos) {
+          // Скроллим вверх
+          header.classList.remove('scroll-up');
+      } else {
+          // Скроллим вниз
+          header.classList.add('scroll-up');
+      }
+  }
+  
+  prevScrollPos = currentScrollPos;
+});
 
 const burgerMenu = document.querySelector('.burger-menu');
-const headerItems = document.querySelector('.header__items ul');
+const menu = document.querySelector('.menu');
 
 burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active');
-    headerItems.classList.toggle('active');
+  isBurgerMenuOpen = !isBurgerMenuOpen;
+  burgerMenu.classList.toggle('active');
+  header.classList.toggle('active');
+  menu.classList.toggle('active');
 });
