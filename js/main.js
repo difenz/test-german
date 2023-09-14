4eec365c0171d7a52d0b1f7562f3dc0d25c51723
German Novikov <difenz1k@gmail.com>
2023-09-14 18:48:05 +0300

    12412

--- js/main.js
+++ js/main.js
@@ -32,3 +32,92 @@ burgerMenu.addEventListener('click', () => {
   header.classList.toggle('active');
   menu.classList.toggle('active');
 });
+
+const openModalButtons = document.querySelectorAll(".show-case-button");
+const closeModalButtons = document.querySelectorAll(".close-modal-btn");
+const modalContainers = document.querySelectorAll(".modal");
+
+openModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+      const modalId = button.getAttribute("data-modal");
+      const modal = document.getElementById(modalId);
+      modal.style.display = "block";
+      setTimeout(() => {
+          modal.classList.add("show");
+      }, 10); // Добавляем небольшую задержку перед добавлением класса "show" для анимации
+  });
+});
+
+closeModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+      const modalId = button.getAttribute("data-modal");
+      const modal = document.getElementById(modalId);
+      modal.classList.remove("show");
+      setTimeout(() => {
+          modal.style.display = "none";
+      }, 300); // Добавляем небольшую задержку перед скрытием модального окна для завершения анимации
+  });
+});
+
+const modal = document.getElementById('modal1');
+const slider = modal.querySelector('.slider');
+const sliderItems = modal.querySelectorAll('.slider-item');
+const prevButton = modal.querySelector('.prev-slide');
+const nextButton = modal.querySelector('.next-slide');
+
+let currentIndex = 0;
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
+    updateSlider();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % sliderItems.length;
+    updateSlider();
+});
+
+updateSlider();
+
+
+const modal1 = document.getElementById('modal1');
+const body = document.body;
+
+function disableBodyScroll() {
+    const scrollY = window.scrollY;
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollY}px`;
+}
+
+function enableBodyScroll() {
+    const scrollY = body.style.top;
+    body.style.position = '';
+    body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+}
+
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modalId = button.getAttribute("data-modal");
+        const modal1 = document.getElementById(modalId);
+        modal1.classList.add("show");
+        modal1.classList.add("modal-overlay"); // Добавляем класс для размытого фона
+        isModalOpen = true; // Устанавливаем, что модальное окно открыто
+        disableBodyScroll(); // Блокируем прокрутку body
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modalId = button.getAttribute("data-modal");
+        const modal1 = document.getElementById(modalId);
+        modal1.classList.remove("show");
+        modal1.classList.remove("modal-overlay"); // Удаляем класс для размытого фона
+        isModalOpen = false; // Устанавливаем, что модальное окно закрыто
+        enableBodyScroll(); // Разблокируем прокрутку body
+    });
+});
